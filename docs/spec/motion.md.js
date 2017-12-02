9ff69300231381b09a6958978604da308fa06ef0
nikogu <644506165@qq.com>
2017-12-02 18:18:29 +0800

    bump 2.13.11

--- docs/spec/motion.md.js
+++ docs/spec/motion.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([61,221],{1374:function(t,n){t.exports={content:["article"],meta:{order:12,title:{"zh-CN":"\u52a8\u6548","en-US":"Motion"},link:"http://motion.ant.design",filename:"docs/spec/motion.md"},toc:["ul"]}}});
+webpackJsonp([61,221],{1320:function(t,n){t.exports={content:["article"],meta:{order:12,title:{"zh-CN":"\u52a8\u6548","en-US":"Motion"},link:"http://motion.ant.design",filename:"docs/spec/motion.md"},toc:["ul"]}}});
